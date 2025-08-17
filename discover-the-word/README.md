f76322eef38f509318abbba9a37f5cd4844c6b22
maplayeroficial <vitorcastro1013@gmail.com>
2025-08-17 18:47:56 -0300

    Tela Inicial e Organização de Componentes

---
+++ discover-the-word/README.md
@@ -0,0 +1,19 @@
+# Discover the Word
+Jogo de descobrir a palavra colocada pelo sistema.
+
+## 🚀 Sobre o projeto
+Este projeto foi desenvolvido com base em uma ideia estudada em um curso de programação.  
+O código foi escrito de forma autoral, servindo como prática e também para portfólio.
+
+## 🛠 Tecnologias utilizadas
+- **HTML5** – estrutura da aplicação
+- **CSS3** – estilização e responsividade
+- **JavaScript (ES6+)** – lógica de interação
+- **React.js** – construção de componentes dinâmicos
+
+## 📚 Objetivo
+Praticar o desenvolvimento de aplicações web modernas, aplicando conceitos de:
+- Estruturação com HTML
+- Estilização com CSS
+- Manipulação e lógica com JavaScript
+- Criação de interfaces reativas com React
---
+++ discover-the-word/src/App.js
@@ -0,0 +1,35 @@
+// CSS
+import './App.css';
+
+// REACT
+import { useCallback, useEffect, useState } from 'react';
+
+// DATA
+import { wordslist } from "./data/words";
+
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordslist);
+
+  console.log(words);
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
