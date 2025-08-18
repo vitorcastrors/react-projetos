8a96903aa7a8f99d1f8b3a46aeb5532344456746
maplayeroficial <vitorcastro1013@gmail.com>
2025-08-18 16:08:47 -0300

    Finalização do projeto Discover the Word

--- discover-the-word/src/App.js
+++ discover-the-word/src/App.js
@@ -17,6 +17,8 @@ const stages = [
   {id: 3, name: "end"},
 ]
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordslist);
@@ -27,25 +29,25 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // Pick a random category
     const categories = Object.keys(words)
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
 
-    console.log(category);
-
     // Pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)]
-    console.log(word);
 
     return {word, category};
-  }
+  }, [words]);
 
   // Starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // Clear all letters
+    clearLetterStates();
+
     // Pick word and pick category
     const { word, category } = pickWordAndCategory();
 
@@ -53,24 +55,75 @@ function App() {
     let wordLetters = word.split("");
     wordLetters = wordLetters.map((l) => l.toLowerCase())
 
-    console.log(wordLetters);
-    console.log(word, category);
-
     // Fill states
     setPickedWord(word);
     setPickedCategory(category);
     setLetters(wordLetters);
 
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
 
   // Process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    // Check if letter has already been utilized
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter))
+    {
+      return;
+    }
+
+    // Push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter))
+    {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters, normalizedLetter
+      ]);
+    }
+
+    else
+    {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters, normalizedLetter
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
   }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  // Check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0 ) {
+      // Reset all states
+      clearLetterStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses, letters, startGame]);
+
+  // Check win condition
+  useEffect(() => {
+    const uniqueLetters = [... new Set(letters)];
+
+    // Win condition
+    if (guessedLetters.length === uniqueLetters.length) {
+      // Add score
+      setScore((actualScore) => actualScore += 100)
+
+      // Restart game with new word
+      startGame();
+    }
+
+  }, [guessedLetters])
 
   // Restarts the game
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
     setGameStage(stages[0].name);
   }
 
@@ -89,7 +142,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
--- discover-the-word/src/components/Game.jsx
+++ discover-the-word/src/components/Game.jsx
@@ -1,6 +1,17 @@
+import { useState, useRef } from "react";
 import "./Game.css";
 
 const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    verifyLetter(letter);
+    setLetter("");
+    letterInputRef.current.focus();
+  }
+
   return (
     <div className="game">
       <h1>Discover the Word</h1>
@@ -23,16 +34,16 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
       </div>
       <div className="letterContainer">
         <p>Tente Adivinhar uma letra da palavra</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef} />
           <button>Jogar!</button>
         </form>
       </div>
       <div className="wrongLettersContainer">
         <p>Letras utilizadas:</p>
-        {wrongLetters.map((letter, i) => {
+        {wrongLetters.map((letter, i) => (
           <span key={i}>{letter}, </span>
-        })}
+        ))}
       </div>
     </div>
   )
